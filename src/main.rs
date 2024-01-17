3267aa54b58140850bf20f8862e0cc1cc269b48c
Dr. Capybara <dustinray313@gmail.com>
2024-01-17 00:54:28 -0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 use crypto_bigint::modular::ConstMontyParams;
-use crypto_bigint::{const_monty_form, impl_modulus, Uint, U128};
+use crypto_bigint::{const_monty_form, impl_modulus, Uint, U128, U64};
 use crypto_bigint::{rand_core::OsRng, NonZero, RandomMod};
 use rand::Rng;
 
 pub fn main() {
     // RSA public exponent
-    let e: Uint<2> = Uint::from(65537_u64);
+    let e: U128 = Uint::from(65537_u64);
 
     // obtained from crypto_primes::generate_safe_prime(Some(32))
-    let p: Uint<1> = Uint::from_be_hex("ECAF5ED6493129D7");
-    let q: Uint<1> = Uint::from_be_hex("98E1FD7AE92F68F3");
+    let p: U64 = Uint::from_be_hex("ECAF5ED6493129D7");
+    let q: U64 = Uint::from_be_hex("98E1FD7AE92F68F3");
 
     // p * q
     const N: &str = "8D5910CC89AFF00D40B1ADB4D0230F15";
     impl_modulus!(Modulus, U128, N);
 
     // Euler's totient
-    let phi: Uint<2> =
-        (p.saturating_sub(&Uint::from(1_u64))).widening_mul(&q.saturating_sub(&Uint::from(1_u64)));
+    let phi = (p - U64::ONE).widening_mul(&(q - U64::ONE));
 
     // private decryption and signing key
     let secret_d = e.inv_mod(&phi).expect("Modular inverse does not exist");
@@ -33,29 +32,27 @@ pub fn main() {
     assert_eq!(m, s.pow(&e), "verification failed");
 
     // public key modulus
-    let n = &NonZero::new(Uint::from_be_hex(N)).unwrap();
+    let n = &NonZero::new(U128::from_be_hex(N)).unwrap();
 
     // prover commits to c = z^e * m mod n for
     // some random non-zero z
-    let mut rng = OsRng;
-    let z_rand = Uint::random_mod(&mut OsRng, n);
+    let z_rand = U128::random_mod(&mut OsRng, n);
     let z = const_monty_form!(z_rand, Modulus);
-
     let c = z.pow(&e).mul(&m);
 
     // Soundness error = 1/2^k
     let k = 20;
     for _ in 0..k {
         // Prover picks a random r_i and keeps it secret
-        let r_i_rand = Uint::random_mod(&mut OsRng, n);
+        let r_i_rand = U128::random_mod(&mut OsRng, n);
         let r_i = const_monty_form!(r_i_rand, Modulus);
         // d = r^e mod n is publicly known
         let d = r_i.pow(&e);
 
         // Verifier picks a random bit b_i
-        let b_i = rng.gen::<bool>();
+        let b_i = OsRng.gen::<bool>();
         // Prover reveals r_i
-        let z_i = if !b_i { r_i } else { r_i.mul(&s).mul(&z) };
+        let z_i = if !b_i { r_i } else { r_i * s * z };
 
         // if b = 0, Prover reveals r_i and verifier checks that
         // r_i^e = d
@@ -63,9 +60,7 @@ pub fn main() {
             assert_eq!(d, z_i.pow(&e),);
         // b_i = 1. Verifier checks that u^e = d * c mod n
         } else {
-            let d_c = d.mul(&c);
-            let u_e = z_i.pow(&e);
-            assert_eq!(d_c, u_e)
+            assert_eq!(d * c, z_i.pow(&e))
         }
     }
 }
